a6cb6dcd06b40ca84cf4285cfb6eaf99158b822c
Jacob <jacob.smith@sapient.com>
2019-02-21 11:31:22 +0000

    using webpack-dev-server (`return false` not working)

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const yargs = require('yargs')
+	.defaults({
+		proxyPort: 3001,
+	}).argv;
+
+module.exports = {
+	devServer: {
+		proxy: {
+			'**': {
+				bypass(req) {
+					if (!req.headers.accept.includes('application/json')) {
+						console.log('do NOT proxy');
+						return false;
+					}
+					console.log('do proxy');
+				},
+				target: `http://localhost:${yargs.proxyPort}`,
+			},
+		},
+	},
+	entry: './app',
+}
